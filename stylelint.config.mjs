a1e4f72052cc3913d57ebd0d049d843ad64c2ec4
Grason Chan <grasonchan@gmail.com>
2025-03-13 23:12:09 +0800

    chore: add config files for formatter and linters

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-high-performance-animation'],
+  rules: {
+    'plugin/no-low-performance-animation-properties': true,
+    'selector-class-pattern': null,
+  },
+};
