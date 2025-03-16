1c41a6aa4399869c01d67a4400c184f3ad8a6369
Grason Chan <grasonchan@gmail.com>
2025-03-16 13:22:34 +0800

    fix: global variable leaks
    dirty global scope because assign function to undeclared variable

--- spotifySpice.js
+++ spotifySpice.js
@@ -1,210 +1,201 @@
-window.addEventListener(
-  'load',
-  (rotateTurntable = () => {
-    const SpicetifyOrigin = Spicetify.Player.origin;
+window.addEventListener('load', function rotateTurntable() {
+  const SpicetifyOrigin = Spicetify.Player.origin;
 
-    if (!SpicetifyOrigin?._state) {
-      setTimeout(rotateTurntable, 250);
-      return;
-    }
+  if (!SpicetifyOrigin?._state) {
+    setTimeout(rotateTurntable, 250);
+    return;
+  }
 
-    const BACKDROP_CONFIG_LABEL = 'Enable blur backdrop';
+  const BACKDROP_CONFIG_LABEL = 'Enable blur backdrop';
 
-    const adModalStyle = document.createElement('style');
-    const STYLE_FOR_AD_MODAL = `
+  const adModalStyle = document.createElement('style');
+  const STYLE_FOR_AD_MODAL = `
 .ReactModalPortal {
   display: none
 }
 `;
-    adModalStyle.innerHTML = STYLE_FOR_AD_MODAL;
+  adModalStyle.innerHTML = STYLE_FOR_AD_MODAL;
 
-    const fadHeartContainer = document.createElement('div');
-    const fadHeart = document.createElement('button');
-    const fadHeartSvg = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'svg'
+  const fadHeartContainer = document.createElement('div');
+  const fadHeart = document.createElement('button');
+  const fadHeartSvg = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'svg'
+  );
+  fadHeartContainer.classList.add('fad-heart-container');
+  fadHeart.classList.add('fad-heart');
+  fadHeartSvg.setAttribute('width', '16');
+  fadHeartSvg.setAttribute('height', '16');
+  fadHeartSvg.setAttribute('viewBox', '0 0 16 16');
+  fadHeart.appendChild(fadHeartSvg);
+  fadHeartContainer.appendChild(fadHeart);
+
+  const songPreviewContainer = document.createElement('div');
+  const previousSong = document.createElement('button');
+  const nextSong = document.createElement('button');
+  songPreviewContainer.classList.add('song-preview');
+  songPreviewContainer.append(previousSong, nextSong);
+
+  const fadArtistSvg = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'svg'
+  );
+  fadArtistSvg.setAttribute('width', '16');
+  fadArtistSvg.setAttribute('height', '16');
+  fadArtistSvg.setAttribute('viewBox', '0 0 16 16');
+  fadArtistSvg.setAttribute('fill', 'currentColor');
+  fadArtistSvg.innerHTML = Spicetify.SVGIcons.artist;
+  const fadAlbumSvg = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'svg'
+  );
+  fadAlbumSvg.setAttribute('width', '16');
+  fadAlbumSvg.setAttribute('height', '16');
+  fadAlbumSvg.setAttribute('viewBox', '0 0 16 16');
+  fadAlbumSvg.setAttribute('fill', 'currentColor');
+  fadAlbumSvg.innerHTML = Spicetify.SVGIcons.album;
+
+  let isFADReady = false;
+  let isPlaying;
+
+  function handleRotate(eventType) {
+    if (eventType === 'load' && !SpicetifyOrigin._state.item) return;
+
+    const coverArt = document.querySelector(
+      '.main-nowPlayingWidget-coverArt > .cover-art'
     );
-    fadHeartContainer.classList.add('fad-heart-container');
-    fadHeart.classList.add('fad-heart');
-    fadHeartSvg.setAttribute('width', '16');
-    fadHeartSvg.setAttribute('height', '16');
-    fadHeartSvg.setAttribute('viewBox', '0 0 16 16');
-    fadHeart.appendChild(fadHeartSvg);
-    fadHeartContainer.appendChild(fadHeart);
+    const fadArt = document.querySelector('#fad-art');
 
-    const songPreviewContainer = document.createElement('div');
-    const previousSong = document.createElement('button');
-    const nextSong = document.createElement('button');
-    songPreviewContainer.classList.add('song-preview');
-    songPreviewContainer.append(previousSong, nextSong);
+    if (
+      (eventType === 'load' && !SpicetifyOrigin._state.isPaused) ||
+      (eventType === 'playpause' && !isPlaying) ||
+      (!eventType && isPlaying)
+    ) {
+      coverArt?.style.setProperty('animation-play-state', 'running');
+      fadArt?.style.setProperty('animation-play-state', 'running');
+      if (eventType) isPlaying = true;
+    } else {
+      coverArt?.style.setProperty('animation-play-state', 'paused');
+      fadArt?.style.setProperty('animation-play-state', 'paused');
+      if (eventType) isPlaying = false;
+    }
+  }
 
-    const fadArtistSvg = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'svg'
+  function handleFadHeart() {
+    const isFadHeartContainer = document.querySelector(
+      '.fad-heart-container'
     );
-    fadArtistSvg.setAttribute('width', '16');
-    fadArtistSvg.setAttribute('height', '16');
-    fadArtistSvg.setAttribute('viewBox', '0 0 16 16');
-    fadArtistSvg.setAttribute('fill', 'currentColor');
-    fadArtistSvg.innerHTML = Spicetify.SVGIcons.artist;
-    const fadAlbumSvg = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'svg'
-    );
-    fadAlbumSvg.setAttribute('width', '16');
-    fadAlbumSvg.setAttribute('height', '16');
-    fadAlbumSvg.setAttribute('viewBox', '0 0 16 16');
-    fadAlbumSvg.setAttribute('fill', 'currentColor');
-    fadAlbumSvg.innerHTML = Spicetify.SVGIcons.album;
 
-    let isFADReady = false;
-    let isPlaying;
+    const stateItem = SpicetifyOrigin._state.item;
 
-    function handleRotate(eventType) {
-      if (eventType === 'load' && !SpicetifyOrigin._state.item) return;
-
-      const coverArt = document.querySelector(
-        '.main-nowPlayingWidget-coverArt > .cover-art'
-      );
-      const fadArt = document.querySelector('#fad-art');
-
-      if (
-        (eventType === 'load' && !SpicetifyOrigin._state.isPaused) ||
-        (eventType === 'playpause' && !isPlaying) ||
-        (!eventType && isPlaying)
-      ) {
-        coverArt?.style.setProperty('animation-play-state', 'running');
-        fadArt?.style.setProperty('animation-play-state', 'running');
-        if (eventType) isPlaying = true;
-      } else {
-        coverArt?.style.setProperty('animation-play-state', 'paused');
-        fadArt?.style.setProperty('animation-play-state', 'paused');
-        if (eventType) isPlaying = false;
-      }
+    if (stateItem.isLocal || stateItem.type === 'ad') {
+      isFadHeartContainer?.remove();
+      return;
     }
 
-    function handleFadHeart() {
-      const isFadHeartContainer = document.querySelector(
-        '.fad-heart-container'
-      );
+    if (!isFadHeartContainer)
+      document
+        .querySelector('#fad-foreground')
+        ?.appendChild(fadHeartContainer);
 
-      const stateItem = SpicetifyOrigin._state.item;
+    if (Spicetify.Player.getHeart()) {
+      fadHeartSvg.innerHTML = Spicetify.SVGIcons['heart-active'];
+      fadHeart.classList.add('checked');
+    } else {
+      fadHeartSvg.innerHTML = Spicetify.SVGIcons.heart;
+      fadHeart.classList.remove('checked');
+    }
+  }
 
-      if (stateItem.isLocal || stateItem.type === 'ad') {
-        isFadHeartContainer?.remove();
-        return;
-      }
+  function handleTracksNamePreview() {
+    const prevTracks = Spicetify.Queue.prevTracks;
+    const currentTrack = Spicetify.Queue.track;
+    const nextTracks = Spicetify.Queue.nextTracks;
 
-      if (!isFadHeartContainer)
-        document
-          .querySelector('#fad-foreground')
-          ?.appendChild(fadHeartContainer);
+    const trackCondition = (element) =>
+      !element.contextTrack.metadata.hidden &&
+      element.provider !== 'ad';
 
-      if (Spicetify.Player.getHeart()) {
-        fadHeartSvg.innerHTML = Spicetify.SVGIcons['heart-active'];
-        fadHeart.classList.add('checked');
-      } else {
-        fadHeartSvg.innerHTML = Spicetify.SVGIcons.heart;
-        fadHeart.classList.remove('checked');
-      }
+    const prevTrack = prevTracks.slice().reverse().find(trackCondition);
+    const nextTrack = nextTracks.find(trackCondition);
+
+    const prevTrackTitle = prevTrack.contextTrack.metadata.title;
+    const currentTrackTitle = currentTrack.contextTrack.metadata.title;
+    const nextTrackTitle = nextTrack.contextTrack.metadata.title;
+
+    if (
+      currentTrackTitle === prevTrackTitle &&
+      currentTrackTitle === nextTrackTitle
+    ) {
+      previousSong.innerHTML = '';
+      nextSong.innerHTML = '';
+    } else {
+      previousSong.innerHTML = `&lt; ${prevTrackTitle}`;
+      nextSong.innerHTML = `${nextTrackTitle} &gt;`;
+    }
+  }
+
+  function handlePopupModalClick(event) {
+    const { PopupModal } = Spicetify;
+    const { target } = event;
+    if (target.closest('.setting-row button.switch')) {
+      PopupModal.hide();
+      handleIcons();
+    }
+  }
+
+  function handleFADBackdrop(event) {
+    const { currentTarget } = event;
+    const fullAppDisplay = document.querySelector('#full-app-display');
+    if (!+localStorage.getItem('enableBlurFad')) {
+      fullAppDisplay.dataset.isBlurFad = 'true';
+      currentTarget.classList.remove('disabled');
+      localStorage.setItem('enableBlurFad', '1');
+    } else {
+      fullAppDisplay.dataset.isBlurFad = 'false';
+      currentTarget.classList.add('disabled');
+      localStorage.setItem('enableBlurFad', '0');
+    }
+  }
+
+  function handleIcons() {
+    const iconsConfig = JSON.parse(
+      localStorage.getItem('full-app-display-config')
+    ).icons;
+
+    if (!iconsConfig) return;
+
+    const isFadArtistSvg = document.querySelector('#fad-artist svg');
+    const isFadAlbumSvg = document.querySelector('#fad-album svg');
+
+    if (SpicetifyOrigin._state.item.type === 'ad') {
+      isFadArtistSvg?.remove();
+      isFadAlbumSvg?.remove();
+
+      return;
     }
 
-    function handleTracksNamePreview() {
-      const prevTracks = Spicetify.Queue.prevTracks;
-      const currentTrack = Spicetify.Queue.track;
-      const nextTracks = Spicetify.Queue.nextTracks;
+    if (!isFadArtistSvg) {
+      const fadArtist = document.querySelector('#fad-artist');
+      const fadArtistTitle = document.querySelector('#fad-artist span');
 
-      trackCondition = (element) =>
-        !element.contextTrack.metadata.hidden &&
-        element.provider !== 'ad';
-
-      const prevTrack = prevTracks
-        .slice()
-        .reverse()
-        .find(trackCondition);
-      const nextTrack = nextTracks.find(trackCondition);
-
-      const prevTrackTitle = prevTrack.contextTrack.metadata.title;
-      const currentTrackTitle =
-        currentTrack.contextTrack.metadata.title;
-      const nextTrackTitle = nextTrack.contextTrack.metadata.title;
-
-      if (
-        currentTrackTitle === prevTrackTitle &&
-        currentTrackTitle === nextTrackTitle
-      ) {
-        previousSong.innerHTML = '';
-        nextSong.innerHTML = '';
-      } else {
-        previousSong.innerHTML = `&lt; ${prevTrackTitle}`;
-        nextSong.innerHTML = `${nextTrackTitle} &gt;`;
-      }
+      fadArtist?.insertBefore(fadArtistSvg, fadArtistTitle);
     }
 
-    function handlePopupModalClick(event) {
-      const { PopupModal } = Spicetify;
-      const { target } = event;
-      if (target.closest('.setting-row button.switch')) {
-        PopupModal.hide();
-        handleIcons();
-      }
+    if (!isFadAlbumSvg) {
+      const fadAlbum = document.querySelector('#fad-album');
+      const fadAlbumTitle = document.querySelector('#fad-album span');
+
+      fadAlbum?.insertBefore(fadAlbumSvg, fadAlbumTitle);
     }
+  }
 
-    function handleFADBackdrop(event) {
-      const { currentTarget } = event;
-      const fullAppDisplay = document.querySelector(
-        '#full-app-display'
-      );
-      if (!+localStorage.getItem('enableBlurFad')) {
-        fullAppDisplay.dataset.isBlurFad = 'true';
-        currentTarget.classList.remove('disabled');
-        localStorage.setItem('enableBlurFad', '1');
-      } else {
-        fullAppDisplay.dataset.isBlurFad = 'false';
-        currentTarget.classList.add('disabled');
-        localStorage.setItem('enableBlurFad', '0');
-      }
-    }
-
-    function handleIcons() {
-      const iconsConfig = JSON.parse(
-        localStorage.getItem('full-app-display-config')
-      ).icons;
-
-      if (!iconsConfig) return;
-
-      const isFadArtistSvg = document.querySelector('#fad-artist svg');
-      const isFadAlbumSvg = document.querySelector('#fad-album svg');
-
-      if (SpicetifyOrigin._state.item.type === 'ad') {
-        isFadArtistSvg?.remove();
-        isFadAlbumSvg?.remove();
-
-        return;
-      }
-
-      if (!isFadArtistSvg) {
-        const fadArtist = document.querySelector('#fad-artist');
-        const fadArtistTitle =
-          document.querySelector('#fad-artist span');
-
-        fadArtist?.insertBefore(fadArtistSvg, fadArtistTitle);
-      }
-
-      if (!isFadAlbumSvg) {
-        const fadAlbum = document.querySelector('#fad-album');
-        const fadAlbumTitle = document.querySelector('#fad-album span');
-
-        fadAlbum?.insertBefore(fadAlbumSvg, fadAlbumTitle);
-      }
-    }
-
-    function handleFADContextMenu() {
-      const { PopupModal } = Spicetify;
-      const configContainer = PopupModal.querySelector('main > div');
-      const settingRow = document.createElement('div');
-      settingRow.classList.add('setting-row');
-      settingRow.innerHTML = `
+  function handleFADContextMenu() {
+    const { PopupModal } = Spicetify;
+    const configContainer = PopupModal.querySelector('main > div');
+    const settingRow = document.createElement('div');
+    settingRow.classList.add('setting-row');
+    settingRow.innerHTML = `
 <label class="col description">${BACKDROP_CONFIG_LABEL}</label>
 <div class="col action">
   <button class="${+localStorage.getItem('enableBlurFad') ? 'switch' : 'switch disabled'}" data-blur-fad>
@@ -212,97 +203,88 @@ window.addEventListener(
   </button>
 </div>
 `;
-      configContainer.insertBefore(
-        settingRow,
-        configContainer.querySelector('.setting-row')
-      );
-      const backdropConfigBtn =
-        configContainer.querySelector('[data-blur-fad]');
-      backdropConfigBtn.addEventListener('click', handleFADBackdrop);
+    configContainer.insertBefore(
+      settingRow,
+      configContainer.querySelector('.setting-row')
+    );
+    const backdropConfigBtn =
+      configContainer.querySelector('[data-blur-fad]');
+    backdropConfigBtn.addEventListener('click', handleFADBackdrop);
+  }
+
+  function handleFADDblClick(event) {
+    const { target } = event;
+    if (target.closest('button')) {
+      event.stopPropagation();
+    }
+  }
+
+  // Todo
+  function handleToggleFad(isActive) {
+    if (isActive) {
+      document.body.append(adModalStyle);
+      return;
     }
 
-    function handleFADDblClick(event) {
-      const { target } = event;
-      if (target.closest('button')) {
-        event.stopPropagation();
-      }
+    const billboard = document.querySelector('#view-billboard-ad');
+
+    billboard?.closest('.ReactModalPortal').remove();
+    adModalStyle.remove();
+  }
+
+  function handleFAD() {
+    const fullAppDisplay = document.querySelector('#full-app-display');
+    fullAppDisplay.appendChild(songPreviewContainer);
+    if (+localStorage.getItem('enableBlurFad'))
+      fullAppDisplay.dataset.isBlurFad = 'true';
+    document
+      .querySelector('#fad-main')
+      .addEventListener('contextmenu', handleFADContextMenu);
+    fullAppDisplay.addEventListener('dblclick', handleFADDblClick);
+    // fullAppDisplay.addEventListener("dblclick", () => handleToggleFad());
+    // handleToggleFad(true);
+    handleIcons();
+    handleFadHeart();
+    handleRotate();
+  }
+
+  function handleFADToggle() {
+    if (!document.body.classList.contains('fad-activated')) {
+      isFADReady = false;
+      return;
     }
+    if (isFADReady) return;
+    handleFAD();
+    isFADReady = true;
+  }
 
-    // Todo
-    function handleToggleFad(isActive) {
-      if (isActive) {
-        document.body.append(adModalStyle);
-        return;
-      }
+  handleRotate('load');
+  handleTracksNamePreview();
 
-      const billboard = document.querySelector('#view-billboard-ad');
-
-      billboard?.closest('.ReactModalPortal').remove();
-      adModalStyle.remove();
-    }
-
-    function handleFAD() {
-      const fullAppDisplay = document.querySelector(
-        '#full-app-display'
-      );
-      fullAppDisplay.appendChild(songPreviewContainer);
-      if (+localStorage.getItem('enableBlurFad'))
-        fullAppDisplay.dataset.isBlurFad = 'true';
-      document
-        .querySelector('#fad-main')
-        .addEventListener('contextmenu', handleFADContextMenu);
-      fullAppDisplay.addEventListener('dblclick', handleFADDblClick);
-      // fullAppDisplay.addEventListener("dblclick", () => handleToggleFad());
-      // handleToggleFad(true);
+  Spicetify.Player.addEventListener('onplaypause', () =>
+    handleRotate('playpause')
+  );
+  Spicetify.Player.addEventListener('songchange', () => {
+    setTimeout(() => {
       handleIcons();
-      handleFadHeart();
       handleRotate();
-    }
+    }, 500);
+  });
+  Spicetify.Player.origin._events.addListener('update', handleFadHeart);
+  Spicetify.Player.origin._events.addListener(
+    'queue_update',
+    handleTracksNamePreview
+  );
 
-    function handleFADToggle() {
-      if (!document.body.classList.contains('fad-activated')) {
-        isFADReady = false;
-        return;
-      }
-      if (isFADReady) return;
-      handleFAD();
-      isFADReady = true;
-    }
+  Spicetify.PopupModal.addEventListener('click', handlePopupModalClick);
 
-    handleRotate('load');
-    handleTracksNamePreview();
+  window.addEventListener('fad-request', handleFADToggle);
 
-    Spicetify.Player.addEventListener('onplaypause', () =>
-      handleRotate('playpause')
-    );
-    Spicetify.Player.addEventListener('songchange', () => {
-      setTimeout(() => {
-        handleIcons();
-        handleRotate();
-      }, 500);
-    });
-    Spicetify.Player.origin._events.addListener(
-      'update',
-      handleFadHeart
-    );
-    Spicetify.Player.origin._events.addListener(
-      'queue_update',
-      handleTracksNamePreview
-    );
-
-    Spicetify.PopupModal.addEventListener(
-      'click',
-      handlePopupModalClick
-    );
-
-    window.addEventListener('fad-request', handleFADToggle);
-
-    fadHeart.addEventListener('click', Spicetify.Player.toggleHeart);
-    previousSong.addEventListener('click', () =>
-      SpicetifyOrigin.skipToPrevious()
-    );
-    nextSong.addEventListener('click', () =>
-      SpicetifyOrigin.skipToNext()
-    );
-  })
-);
+  fadHeart.addEventListener('click', Spicetify.Player.toggleHeart);
+  previousSong.addEventListener('click', () =>
+    SpicetifyOrigin.skipToPrevious()
+  );
+  nextSong.addEventListener('click', () =>
+    SpicetifyOrigin.skipToNext()
+  );
+});
