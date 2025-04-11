76239126565611fe3c2e98a0028fefeb408cf166
Grason Chan <grasonchan@gmail.com>
2025-04-11 22:07:42 +0800

    feat(Component): add common SVGButton Component

--- SpotifySpice/theme.js
+++ SpotifySpice/theme.js
@@ -61,6 +61,47 @@
       () => Spicetify.Queue
     );
 
+  const SVGButton = ({
+    text,
+    icon,
+    svgPriority = true,
+    svgProps = {},
+    className,
+    style = {},
+    disabled = false,
+    onClick,
+  }) => {
+    return react.createElement(
+      'button',
+      {
+        className: classnames('common-svg-button', className),
+        style,
+        disabled,
+        onClick,
+      },
+      react.createElement('svg', {
+        width: 16,
+        height: 16,
+        viewBox: '0 0 16 16',
+        fill: 'currentColor',
+        ...svgProps,
+        dangerouslySetInnerHTML: {
+          __html: icon,
+        },
+      }),
+      text &&
+        react.createElement(
+          'span',
+          {
+            style: {
+              order: svgPriority ? 0 : -1,
+            },
+          },
+          text
+        )
+    );
+  };
+
   const Heart = () => {
     const { COLLECTED, DISABLED } = HEART_STATUS;
 
