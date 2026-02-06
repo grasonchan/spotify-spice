5a49772a5ee20a8ccca9d34370371dff7580f84b
Grason Chan <grasonchan@gmail.com>
2026-02-07 00:33:32 +0800

    build: add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'theme.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
