d65e757fb9594bafc48f2ad359cbdb2743b82efa
Grason Chan <grasonchan@gmail.com>
2026-03-07 00:29:25 +0800

    build: integrate SpicetifyDeployPlugin in dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,14 @@
 import { merge } from 'webpack-merge';
-import common from './webpack.common.js';
+import SpicetifyDeployPlugin from './spicetify-deploy-webpack-plugin.js';
+import common, { THEME_NAME } from './webpack.common.js';
 
 export default merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
+  plugins: [
+    new SpicetifyDeployPlugin({
+      theme: THEME_NAME,
+      scheme: 'spotify-spice',
+    }),
+  ],
 });
