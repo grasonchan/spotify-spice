6bd302c48140acb7c8cf4f7df5684b874c996ec1
Grason Chan <grasonchan@gmail.com>
2026-05-23 21:47:57 +0800

    chore(const): define texts for Now Playing Bar

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,6 +2,9 @@ export const CONFIG_KEY = 'spotify-spice';
 
 export const LEGACY_CONFIG_KEY = 'enableBlurFad';
 
+export const GREETING = 'Hi~ Spotify ☺️';
+export const NO_MORE_TRACKS = 'No More Tracks';
+
 export const THEMES = {
   LIGHT: 'light',
   DARK: 'dark',
