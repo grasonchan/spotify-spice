082f8c229289fe0aec37874b8b52f6e5427ff1f6
Grason Chan <grasonchan@gmail.com>
2026-05-24 21:00:55 +0800

    chore(stylelint): enforce import-notation to string style

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,6 +5,7 @@ export default {
   plugins: ['stylelint-high-performance-animation'],
   rules: {
     'plugin/no-low-performance-animation-properties': true,
+    'import-notation': 'string',
     'selector-class-pattern': null,
     'no-descending-specificity': null,
   },
