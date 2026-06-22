627765b55d8cbfb79bc583529a0e0171b897cfc3
Grason Chan <grasonchan@gmail.com>
2026-06-22 18:59:02 +0800

    feat(vinyl): cleanup play state CSS variable on unmount

--- src/extensions/vinyl/use-vinyl-play-state.js
+++ src/extensions/vinyl/use-vinyl-play-state.js
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
 import { usePlayStatus } from '@/hooks/host/use-play-status.js';
 
+const VINYL_PLAY_STATE = '--vinyl-play-state';
+
 export const useVinylPlayState = () => {
   const playStatus = usePlayStatus();
 
   useEffect(() => {
     document.documentElement.style.setProperty(
-      '--vinyl-play-state',
+      VINYL_PLAY_STATE,
       playStatus ? 'running' : 'paused'
     );
   }, [playStatus]);
+
+  useEffect(
+    () => () =>
+      document.documentElement.style.removeProperty(VINYL_PLAY_STATE),
+    []
+  );
 };
